67fb79e7ae8ed6c055a2ad5c27c62ee3ab46becc
Keiji O <meekunn@gmail.com>
2023-09-13 03:47:34 -0700

    createDeck resolver functional

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -27,7 +27,7 @@ module.exports = {
 
     return req;
   },
-  signToken({ username, email, _id }) {
+  signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
