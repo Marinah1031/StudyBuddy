1e50978d40417b2e2a8c526eb568b62216d1538a
Keiji O <meekunn@gmail.com>
2023-09-15 20:32:51 -0700

    login work on login.js

--- client/src/components/Login/LoginStyles/Login.js
+++ client/src/components/Login/LoginStyles/Login.js
@@ -1,68 +1,96 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Navbar from "../../Navbar/Navbar";
 import "./Login.css";
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER, ADD_USER } from "../../../utils/mutations";
+import Auth from "../../../utils/auth";
 
-
-function Login() {
+function Login(props) {
   useEffect(() => {
-    const handleSignUpClick = ( event ) => {
-      event.preventDefault();
 
-      const wrapper = document.querySelector('.wrapper');
-      wrapper.classList.add('animate-signIn');
-      wrapper.classList.remove('animate-signUp');
+    const handleSignUpClick = () => {
+      // event.preventDefault();
+
+      const wrapper = document.querySelector(".wrapper");
+      wrapper.classList.add("animate-signIn");
+      wrapper.classList.remove("animate-signUp");
+    };
+    const handleSignInClick = () => {
+      // event.preventDefault();
+      const wrapper = document.querySelector(".wrapper");
+      wrapper.classList.add("animate-signUp");
+      wrapper.classList.remove("animate-signIn");
     };
 
-    const handleSignInClick = (event ) => {
-      event.preventDefault();
-      const wrapper = document.querySelector('.wrapper');
-      wrapper.classList.add('animate-signUp');
-      wrapper.classList.remove('animate-signIn');
-    };
+    const signUpLink = document.querySelector(".signUp-link");
+    const signInLink = document.querySelector(".signIn-link");
 
-    const signUpLink = document.querySelector('.signUp-link');
-    const signInLink = document.querySelector('#login');
-
-    signUpLink.addEventListener('submit', handleSignUpClick);
-    signInLink.addEventListener('submit', handleSignInClick);
+    signUpLink.addEventListener("click", handleSignUpClick);
+    signInLink.addEventListener("click", handleSignInClick);
 
     // Cleanup: Remove event listeners when the component unmounts
     return () => {
-      signUpLink.removeEventListener('submit', handleSignUpClick);
-      signInLink.removeEventListener('submit', handleSignInClick);
+      signUpLink.removeEventListener("click", handleSignUpClick);
+      signInLink.removeEventListener("click", handleSignInClick);
     };
   }, []);
 
+
+  //handles the login form
+  const [loginFormState, setLoginFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  const loginHandleChange = (event) => {
+    const { name, value } = event.target;
+    setLoginFormState({
+      ...loginFormState,
+      [name]: value,
+    });
+  };
+
+const handleSignInSubmit = async (event) => {
+  event.preventDefault();
+  try {
+    console.log(loginFormState)
+const loginResponse = await login({ variables: {email:loginFormState.email, password: loginFormState.password}});
+const token = loginResponse.data.login.token;
+Auth.login(token);
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
   return (
     <section>
-        <Container className="navbar">
-            <Navbar/>
-        </Container>
-      <Container className="type-container">
-        <div className="wrapper">
-          <div className="form-wrapper sign-up">
-            <form action="">
+      <Container className='navbar'>
+        <Navbar />
+      </Container>
+      <Container className='type-container'>
+        <div className='wrapper'>
+          <div className='form-wrapper sign-up'>
+            <form id='signup'>
               <h2>Sign Up</h2>
-              <div className="input-group">
-                <input type="text" required />
-                <label htmlFor="">Username</label>
+              <div className='input-group'>
+                <input type='text' required />
+                <label htmlFor=''>Username</label>
               </div>
-              <div className="input-group">
-                <input type="email" required />
-                <label htmlFor="">Email</label>
+              <div className='input-group'>
+                <input type='email' required />
+                <label htmlFor=''>Email</label>
               </div>
-              <div className="input-group">
-                <input type="password" required />
-                <label htmlFor="">Password</label>
+              <div className='input-group'>
+                <input type='password' required />
+                <label htmlFor=''>Password</label>
               </div>
-              <button type="submit" className="btn">
+              <button type='submit' className='btn'>
                 Sign Up
               </button>
-              <div className="sign-link">
+              <div className='sign-link'>
                 <p>
                   Already have an account?{" "}
-                  <a href="#" className="signIn-link">
+                  <a className='signIn-link'>
                     Sign In
                   </a>
                 </p>
@@ -70,27 +98,35 @@ function Login() {
             </form>
           </div>
 
-          <div className="form-wrapper sign-in">
-            <form id="login">
+          <div className='form-wrapper sign-in'>
+            <form onSubmit={handleSignInSubmit}>
               <h2>Login</h2>
-              <div className="input-group">
-                <input type="text" required />
-                <label htmlFor="">Username</label>
+              <div className='input-group'>
+                <input type='email'
+                name="email"
+                id = "email"
+                onChange={loginHandleChange} required />
+                <label htmlFor='email'>Email</label>
               </div>
-              <div className="input-group">
-                <input type="password" required />
-                <label htmlFor="">Password</label>
+              <div className='input-group'>
+                <input type='password' name='password' id="password" onChange={loginHandleChange} required />
+                <label htmlFor='password'>Password</label>
               </div>
-              <div className="forgot-pass">
-                <a href="#">Forgot Password?</a>
+              <div className='forgot-pass'>
+                <a href='#'>Forgot Password?</a>
               </div>
-              <button type="submit" className="btn">
+              {error ? (
+          <div>
+            <p className="error-text">Entered credentials do not match. Please try again.</p>
+          </div>
+        ) : null}
+              <button type='submit' className='btn'>
                 Login
               </button>
-              <div className="sign-link">
+              <div className='sign-link'>
                 <p>
                   Don't have an account?{" "}
-                  <a href="#" className="signUp-link">
+                  <a className='signUp-link'>
                     Sign Up
                   </a>
                 </p>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,15 +1,14 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
     }
   }
+}
 `;
 
 export const ADD_USER = gql`
