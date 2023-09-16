12618bd1620f08addebb3b7813946fa22edcda6e
Muzzammil Nawab <90220760+YaBoiAli@users.noreply.github.com>
2023-09-15 22:28:26 -0500

    fixed merge conflicts

--- client/src/pages/CardPage.js
+++ client/src/pages/CardPage.js
@@ -1,9 +1,3 @@
-//previous button
-//next button
-
-//flip button
-
-// src/components/FlippingCard.js
 import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { FIND_SINGLE_DECK } from '../utils/querys';
@@ -69,66 +63,8 @@ const CardPage = () => {
       ) : (
         <p>No Cards to show</p>
       )}
-    <section>
-      <h1>title</h1>
-      <h2>definition</h2>
-      {<div>
-        <button onClick={prevCard}>Previous</button>
-        <button onClick={nextCard}>Next</button>
-        {cards.length > 1 ? cards.map((card, i) => (
-          <CardComponent currentIndex={currentIndex} key={i} currentCard={i} term={card.term} definition={card.definition}/>
-        ))
-          : "no Cards to show"}
-      </div>}
     </section>
   );
 };
 
 export default CardPage;
-
-
-// function CardComponent({ title, definition }) {
-//   const [isFlipped, setIsFlipped] = useState(false);
-
-//   const currentCard = cards[currentIndex];
-
-//   const flipCard = () => {
-//     setIsFlipped(!isFlipped);
-//   };
-
-//   const nextCard = () => {
-//     setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
-//     setIsFlipped(false);
-//   };
-
-//   const prevCard = () => {
-//     setCurrentIndex((prevIndex) =>
-//       prevIndex === 0 ? cards.length - 1 : prevIndex - 1
-//     );
-//     setIsFlipped(false);
-//   };
-
-//   return (
-//     <section>
-//       <h1>title</h1>
-//       <h2>definition</h2>
-//       {  <div>
-//            <button onClick={prevCard}>Previous</button>
-//             <button onClick={nextCard}>Next</button>
-//     {cards.length > 1 ? cards.map((card) => (
-//             <div className={`card ${isFlipped ? 'flipped' : ''}`}>
-//             <div className="card-inner front">
-//               <button onClick={flipCard}>Flip</button>
-//               <p>{card.term}</p>
-//             </div>
-//             <div className="card-inner back">
-//               <button onClick={flipCard}>Flip</button>
-//               <p>{card.definition}</p>
-//             </div>
-
-//           </div>
-//         ))
-//       :"no Cards to show"}
-//    </div>}
-//     </section>
-//   )
