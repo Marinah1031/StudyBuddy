e5c7e29d75d41b8644813f8d63b7ccd9a6a346e5
Muzzammil Nawab <90220760+YaBoiAli@users.noreply.github.com>
2023-09-16 17:31:14 -0500

    navbar, and css of the main page

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,34 +1,26 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import './cards.css';
+import styles from './main.module.css';
 import { Link } from 'react-router-dom';
 
-
-function Deck ({ deckName, description, createdBy, deckId }) {
- 
-
-
-        return (
-            <section>
-                   <Navbar />
-                   <button>
-                   <Link to={`/saved/${deckId}`}>
-                <div>
-                  <p>{deckName}</p>
-                </div>
-                <div>
-                  <p>{description}</p>
-                </div>
-                <div>
-                  <p>{createdBy}</p>
-                </div>
-                <div>
-                  <p>{deckId}</p>
-                </div>
-                </Link>
-                </button>
-            </section>
-          );
+function Deck({ deckName, description, createdBy, deckId }) {
+  return (
+    <section key={deckId}>
+      <Navbar />
+      <div className={styles['deck-container']}>
+        <button className={styles['deck-link']}>
+          <Link to={`/saved/${deckId}`} className={styles['deck-link']}>
+            <div className={styles['deck-info']}>
+              <p className={styles['deck-name']}>{deckName}</p>
+              <p className={styles['description']}>{description}</p>
+              <p className={styles['created-by']}>Created by: {createdBy}</p>
+              <p className={styles['deck-id']}>Deck ID: {deckId}</p>
+            </div>
+          </Link>
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Deck;
