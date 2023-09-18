8a63697c3e7fa94ec632ee5f99575082b90af5c2
Marinah1031 <marinahtam@gmail.com>
2023-09-18 10:33:13 -0700

    adding commentary

--- client/src/components/cards.js
+++ client/src/components/cards.js
@@ -19,12 +19,16 @@ function CardComponent({ term, definition, currentIndex, currentCard, deckId}) {
   };
 
   return (
-   
+    // This is a JSX section element with inline CSS to control its display.
     <section style={{ display: currentIndex === currentCard ? '' : 'none' }}>
+          {/* This is a button element with a conditional class 'flipped' based on the 'isFlipped' state.
+         It also has an onClick event handler 'handleClick' attached. */}
       <button className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
+          {/* This is the front side of the card with a title displayed. */}
         <div className="card-inner front">
           <p className='title-card-inner-front'>{term}</p>
         </div>
+          {/* This is the back side of the card with the definition displayed. */}
         <div className="card-inner back">
           <p className='title-card-inner-back'>{definition}</p>
         </div>
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,29 +19,31 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
+        // If there's a user in the context, fetch user data and return it
         const userData = await User.findOne({ _id: context.user._id }).select(
           "-__v -password"
         );
 
         return userData;
       }
-
+      // If there's no user in the context, throw a "You need to be logged in" error
       throw needLogin;
     },
 
     allDecks: async () => Deck.find(),
     allUsers: async () => User.find(),
 
-    getUser: async (parent, { userID}) => User.findOne({ _id: userID }),
+    getUser: async (parent, { userID }) => User.findOne({ _id: userID }),
     viewDeck: async (parent, { deckID }) => Deck.findOne({ _id: deckID }),
     viewUserDecks: async (parent, { userID }) => Deck.find({ createdBy: userID }),
     getUserDecks: async (parent, args, context) => {
       if (context.user) {
+        // If there's a user in the context, fetch decks created by that user
         const uerDecks = await Deck.find({ createdBy: context.user._id });
 
         return uerDecks;
       }
-
+      // If there's no user in the context, throw a "You need to be logged in" error
       throw needLogin;
     },
 
@@ -49,31 +51,37 @@ const resolvers = {
 
   Mutation: {
     addUser: async (parent, args) => {
+      // Create a new user and generate a token
       const user = await User.create(args);
       const token = signToken(user);
 
       return { token, user };
     },
     login: async (parent, { email, password }) => {
+      // Find the user by email
       const user = await User.findOne({ email });
 
       if (!user) {
+        // If the user does not exist, throw an "Email problem" error
         throw loginError1;
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
+         // If the password is incorrect, throw a "PW problem" error
         throw loginError2;
       }
 
+       // If email and password are correct, generate a token and return it
       const token = signToken(user);
       return { token, user };
     },
-    createDeck: async (parent, {deckName, description}, context) => {
+    createDeck: async (parent, { deckName, description }, context) => {
       if (context.user) {
+        // If there's a user in the context, create a new deck with user's ID
         const userID = context.user._id;
-        const newDeck = await Deck.create({deckName, description, createdBy: userID});
+        const newDeck = await Deck.create({ deckName, description, createdBy: userID });
 
         return newDeck;
       }
@@ -81,36 +89,36 @@ const resolvers = {
     },
     removeDeck: async (parent, { deckId }, context) => {
       const deck = await Deck.findOne({ _id: deckId });
-    
+
       if (!deck) {
         // Handle case where deck with the given deckId does not exist
         throw new Error('Deck not found');
       }
-    
+
       // console.log(deck.createdBy);
       // console.log(context.user._id);
       if (deck.createdBy.toString() !== context.user._id) {
         // If the deck's createdBy doesn't match the user's id, throw an error
         throw new Error(ownership);
       }
-    
+
       // If the createdBy id matches the user's id, proceed with deletion
       const deletedDeck = await Deck.findOneAndDelete({
         _id: deckId,
       });
-    
+
       return deletedDeck;
     },
 
-    addCard: async (parent, {deckId, term, definition}) => {
+    addCard: async (parent, { deckId, term, definition }) => {
       const deck = await Deck.findOneAndUpdate(
-        {_id: deckId},
-        {$addToSet: {cards: { term, definition }}}, 
+        { _id: deckId },
+        { $addToSet: { cards: { term, definition } } },
         {
           new: true,
           runValidators: true,
         })
-        return deck;
+      return deck;
     },
 
     removeCard: async (parent, { deckId, cardId }, context) => {
